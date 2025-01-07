ba68a1131f32573eb91eab8d8c70bea9f30aac23
Vitoria <mvitoriaberto2@gmail.com>
2025-01-07 20:04:42 -0300

    feat: add basic structure for Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 //import React from 'react'
+import Hero from "./components/hero"
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5x1 text-orange-500 font-bold">Bem-Vindo ao Awwards</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   
   )
